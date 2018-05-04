3dc12f85cf95e23a5123dc9fc2efa5f0a377dc0b
Jim Knopf <jsk4@mac.com>
2018-05-03 21:07:48 -0700

    User signups completed

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -11,10 +11,12 @@ RSpec.describe WelcomeController, type: :controller do
   end
 
   describe "GET about" do
-    it "renders teh about template" do
+    it "renders the about template" do
       get :about
       expect(response).to render_template("about")
     end
   end
+
+
 
 end
